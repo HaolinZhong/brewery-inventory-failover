e985ed47ca853691a3ab896be568eed0d4f5f4ab
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-19 20:38:27 -0400

    init project

---
+++ src/main/java/hz/spring/breweryinventoryfailover/BreweryInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package hz.spring.breweryinventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryInventoryFailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryInventoryFailoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hz/spring/breweryinventoryfailover/BreweryInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package hz.spring.breweryinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
